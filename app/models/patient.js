70861d25e26de3b90dc95d587848535bb1677855
Charles Lowell <cowboyd@frontside.io>
2016-10-12 21:12:29 -0500

    add a Kg weight conversion.

--- app/models/patient.js
+++ app/models/patient.js
@@ -6,6 +6,9 @@ export default DS.Model.extend({
   name: DS.attr(),
   birthday: DS.attr('date'),
   weight: DS.attr('number'),
+  weightKg: Ember.computed('weight', function() {
+    return 0.4536 * this.get('weight');
+  }),
   targetHgB: DS.attr('number'),
   avatar: DS.attr(),
   age: Ember.computed('birthday', function() {
