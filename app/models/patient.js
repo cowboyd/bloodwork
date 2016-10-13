18394c631bf98ce5eeacf81f85fb6b958b91ac8d
Charles Lowell <cowboyd@frontside.io>
2016-10-12 21:00:43 -0500

    add patient index with age.

--- app/models/patient.js
+++ app/models/patient.js
@@ -1,9 +1,14 @@
+import Ember from 'ember';
 import DS from 'ember-data';
+import moment from 'moment';
 
 export default DS.Model.extend({
   name: DS.attr(),
-  age: DS.attr('number'),
+  birthday: DS.attr('date'),
   weight: DS.attr('number'),
   targetHgB: DS.attr('number'),
-  avatar: DS.attr()
+  avatar: DS.attr(),
+  age: Ember.computed('birthday', function() {
+    return moment().diff(this.get('birthday'), 'years');
+  })
 });
--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -1,3 +1,8 @@
 export default function configure(server) {
-  server.createList('patient', 2);
+  let phil = server.create('patient', {
+    name: "Phil Lowell",
+    birthday: new Date(1931, 7, 9),
+    weight: 185,
+    targetHgB: 9.5
+  });
 }
